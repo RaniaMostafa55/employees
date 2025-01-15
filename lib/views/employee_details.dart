2446ea65fb635cc721cebf2e9b99e0b358b9b1ee
RaniaMostafa55 <raniamostafah55@gmail.com>
2025-01-15 19:35:00 +0200

    handling details

--- lib/views/employee_details.dart
+++ lib/views/employee_details.dart
@@ -6,8 +6,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/employee_model.dart';
 
 class EmployeeDetails extends StatefulWidget {
-  final Employee employee;
-  const EmployeeDetails({required this.employee, super.key});
+  final int index;
+  const EmployeeDetails({required this.index, super.key});
 
   @override
   State<EmployeeDetails> createState() => _EmployeeDetailsState();
@@ -15,14 +15,18 @@ class EmployeeDetails extends StatefulWidget {
 
 class _EmployeeDetailsState extends State<EmployeeDetails> {
   List<Employee> employees = [];
+  bool isLoading = false;
   //a function to get employees cached data and store it in "employees" list
   getEmployees() async {
+    isLoading = true;
+    setState(() {});
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     String? employeesData = prefs.getString("employeesData");
     var cachedData = jsonDecode(employeesData!);
     for (var data in cachedData) {
       employees.add(Employee.fromJson(data));
     }
+    isLoading = false;
     setState(() {});
   }
 
@@ -44,36 +48,52 @@ class _EmployeeDetailsState extends State<EmployeeDetails> {
           ),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                //show employee image
-                child: Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: Image(image: NetworkImage(widget.employee.imageUrl)),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      //show employee image
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10)),
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        child: Image(
+                            image:
+                                NetworkImage(employees[widget.index].imageUrl)),
+                      ),
+                    ),
+                    //show all employee details
+                    DetailsCard(
+                        title: "Id", data: "${employees[widget.index].id}"),
+                    DetailsCard(
+                        title: "Name",
+                        data:
+                            "${employees[widget.index].firstName} ${employees[widget.index].lastName}"),
+                    DetailsCard(
+                        title: "Age", data: "${employees[widget.index].age}"),
+                    DetailsCard(
+                        title: "Date of Birth",
+                        data: employees[widget.index].dob),
+                    DetailsCard(
+                        title: "Address",
+                        data: employees[widget.index].address),
+                    DetailsCard(
+                        title: "Contact Number",
+                        data: employees[widget.index].contactNumber),
+                    DetailsCard(
+                        title: "Email", data: employees[widget.index].email),
+                    DetailsCard(
+                        title: "Salary",
+                        data: "${employees[widget.index].salary}")
+                  ],
                 ),
-              ),
-              //show all employee details
-              DetailsCard(title: "Id", data: "${widget.employee.id}"),
-              DetailsCard(
-                  title: "Name",
-                  data:
-                      "${widget.employee.firstName} ${widget.employee.lastName}"),
-              DetailsCard(title: "Age", data: "${widget.employee.age}"),
-              DetailsCard(title: "Date of Birth", data: widget.employee.dob),
-              DetailsCard(title: "Address", data: widget.employee.address),
-              DetailsCard(
-                  title: "Contact Number", data: widget.employee.contactNumber),
-              DetailsCard(title: "Email", data: widget.employee.email),
-              DetailsCard(title: "Salary", data: "${widget.employee.salary}")
-            ],
-          ),
-        ));
+              ));
   }
 }
